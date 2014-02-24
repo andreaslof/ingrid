d007c881c31ac109cfd3fc7f74546798b97453a2
Andreas Löf Hermansson <andreas.lof@kurppahosk.com>
2014-02-24 11:14:26 +0100

    Switched build system to Gulp

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp')
+  , compass = require('gulp-compass')
+  , minifyCSS = require('gulp-minify-css')
+  , watcher = gulp.watch('sass/**/*.scss', ['default']);;
+
+gulp.task('default', function () {
+  gulp.src('./sass/**/*.scss')
+      .pipe(compass({
+        style: 'compressed',
+        css: 'css',
+        sass: 'sass'
+      }))
+      .on('success', function (msg) {
+        console.log('Compiled '+msg);
+      })
+      .on('error', function (err) {
+        console.log('Carp! An error occured; '+err);
+      });
+});
+
+watcher.on('change', function (evt) {
+  console.log('[watcher] File '+evt.path.replace(/.*(?=sass)/,'')+' was '+evt.type+', compiling...');
+})
